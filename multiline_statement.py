6cca904f4be398a8bd661fb5fdc315b066cfc715
Suchit Gupta <suchitgupta027@gmail.com>
2023-07-19 22:05:11 +0530

    how to write code in multiple lines

---
+++ multiline_statement.py
@@ -0,0 +1,17 @@
+tv = 15
+mobile = 20
+tablet = 30
+
+# notice how \ is used
+total = tv + \
+    mobile + \
+    tablet
+print(total)
+
+# statements in brackets of all kinds don't need \ to continue code to next line
+days = ['Mon', 'Tues', 'Wed',
+        'Thurs', 'Fri', 'Sat']
+print(days)
+
+# ; can be used to put multiple lines in single lines if neither statement start a new block
+tv = 1; name = "Rahul"; print(name); print("Welcome")
