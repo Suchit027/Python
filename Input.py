7822f8c5173f04e7ca3ef6829bd89796c5bb075c
Suchit Gupta <suchitgupta027@gmail.com>
2023-07-19 22:23:59 +0530

    taking user input

---
+++ Input.py
@@ -0,0 +1,7 @@
+name = input("Enter your name here")
+age = int(input("Enter age "))
+print("Enter anything")
+
+# notice how input is used to create a new line input
+anything = input()
+print(anything)
