33de2b8bbeea27da2b6a3dc264cfc30c232f512c
Suchit Gupta <suchitgupta027@gmail.com>
2023-07-22 17:50:51 +0530

    upgrading comments

--- graphing/graphing1.py
+++ graphing/graphing1.py
@@ -1,7 +1,10 @@
 import seaborn
 from matplotlib import pyplot
 
+# loading existing dataset
 dataset = seaborn.load_dataset('tips')
+
+# regular plotting; naming x and y-axis and specifying data
 seaborn.regplot(x='total_bill', y='tip', data=dataset)
 pyplot.xlabel = 'Total Bill'
 pyplot.ylabel = 'Bill Tips'
--- graphing/graphing2.py
+++ graphing/graphing2.py
@@ -2,5 +2,7 @@ import seaborn
 from matplotlib import pyplot
 
 dataset = seaborn.load_dataset('iris')
+
+# pairwise comparison
 seaborn.pairplot(dataset, kind='scatter')
 pyplot.show()
