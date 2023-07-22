fefef7498115fd177f75b8da408211614770d1b8
Suchit Gupta <suchitgupta027@gmail.com>
2023-07-22 22:10:41 +0530

    trying to learn about graphs

--- graphing/graphing5.py
+++ graphing/graphing5.py
@@ -8,9 +8,18 @@ seaborn.set_style('whitegrid')
 data = numpy.random.multivariate_normal([0, 0], [[5, 2], [2, 2]], size=2000)
 data = pandas.DataFrame(data, columns=['x', 'y'])
 
-seaborn.displot(data['x'])
-seaborn.displot(data['y'])
+seaborn.distplot(data['x'])
+seaborn.distplot(data['y'])
 pyplot.show()
+
+# for kernel density estimation
 for col in 'xy':
     seaborn.kdeplot(data[col], fill=True)
 pyplot.show()
+
+# passing full 2 D data set to kdeplot
+seaborn.kdeplot(data)
+pyplot.show()
+
+with seaborn.axes_style('white'):
+    seaborn.jointplot("x", "y", data=data, kind='kde')
