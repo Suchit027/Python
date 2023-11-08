5078011a67508bac731f2355946a9d8dd8093740
Suchit Gupta <suchitgupta027@gmail.com>
2023-11-08 17:25:04 +0530

    confusion matrix

--- gowrishankar book/book questions/P_11.7.py
+++ gowrishankar book/book questions/P_11.7.py
@@ -2,10 +2,11 @@ class default:
     def __init__(self, test1=1, test2=2):
         self.test1 = test1
         self.test2 = test2
+        self.test = "hello"
 
     def display(self):
-        print(self.test1  + self.test2)
-
+        print(self.test1 + self.test2)
+        print(self.test)
 
 test = default(3)
 test.display()
